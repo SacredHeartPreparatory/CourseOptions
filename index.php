34ba7b1914085c40bf2f35c99c41e4c1853c4198
dylanmodesitt <dylanmodesitt@gmail.com>
2016-03-03 16:48:03 -0800

    Added basic file structure

---
+++ index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="description" content="This is the personal website and portfolio of Dylan Modesitt">
+	<meta name="author" content="">
+
+	<title>Dylan Modesitt</title>
+
+	<!-- Plugin CSS -->
+	<link href="sources/css/normalize.css" rel="stylesheet" media="screen">
+	<link href="sources/bootstrap/css/bootstrap.min.css" rel="stylesheet" media="screen">
+	<link href="sources/css/font-awesome.min.css" rel="stylesheet" media="screen">
+	<link href="sources/css/animate.css" rel="stylesheet" media="screen">
+
+
+	<!-- Custom styles CSS -->
+	<link href="sources/css/the.css" rel="stylesheet" media="screen">
+
+
+</head>
+<body>
+	<!-- Designed by SHP Tech Ambassadors -->
+	<section id="hero">
+		<div class="container">
+			<div class="row col-xs-12">
+				<div class="page-header">
+		  			<h1>Course Options <small>SHP</small></h1>
+				</div>
+			</div>
+		</div>
+	</section><!-- /#hero -->
+
+	<section id="content">
+		<div class="container">
+			<div class="row col-xs-12">
+				<!-- Content -->
+			</div>
+		</div>
+	</section><!-- /#content -->
+
+
+
+
+	<!-- Javascript files -->
+
+	<!-- Javascript Libraries -->
+	<script src="sources/js/modernizr.custom.js"></script>
+
+	<!-- Javascript Custom -->
+	<script src="sources/js/the.js"></script>
+
+
+</body>
+</html>
